f2930f2c99ec9287e00af2674d85443d74a605ed
Matthew Baldwin <matt.d.baldwin@gmail.com>
2020-03-19 14:05:09 -0700

    Changed settings and text

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,90 +1,129 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Contact extends Component {
-  render() {
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
-    }
+   constructor(props) {
+      super(props);
+      this.state = {
+      contactName: '',
+      contactEmail: '',
+      contactSubject: '',
+      contactMessage: '',
+      }
+     }
 
-    return (
-      <section id="contact">
+   handleSubmit(e){
+      e.preventDefault();
+      axios({
+        method: "POST", 
+        url:"http://localhost:3002/send", 
+        data:  this.state
+      }).then((response)=>{
+        if (response.data.status === 'success'){
+          alert("Message Sent."); 
+          this.resetForm()
+        }else if(response.data.status === 'fail'){
+          alert("Message failed to send.")
+        }
+      })
+   }
+  
+   resetForm(){
+      
+       this.setState({contactName: '', email: '', message: ''})
+   }
 
-         <div className="row section-head">
+   render() {
 
-            <div className="two columns header-col">
+      if(this.props.data){
+         var name = this.props.data.name;
+         var email = this.props.data.email;
+      }
 
-               <h1><span>Get In Touch.</span></h1>
+      return (
+         <section id="contact">
+
+            <div className="row section-head">
+
+               <div className="two columns header-col">
+
+                  <h1><span>Get In Touch.</span></h1>
+
+               </div>
+
+               <div className="ten columns">
+                     
+                     <p className="lead">{name}</p>
+                     <p className="lead">{email}</p>
+
+               </div>
 
             </div>
 
-            <div className="ten columns">
+            <div className="row">
+               <div className="eight columns">
 
-                  <p className="lead">{message}</p>
+                  <form action="" method="post" id="contactForm" name="contactForm">
+                  <fieldset>
 
+                     <div>
+                        <label htmlFor="contactName">Name <span className="required">*</span></label>
+                        <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+                     </div>
+
+                     <div>
+                        <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+                        <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+                     </div>
+
+                     <div>
+                        <label htmlFor="contactSubject">Subject</label>
+                        <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+                     </div>
+
+                     <div>
+                        <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                        <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     </div>
+
+                     <div>
+                        <button className="submit">Submit</button>
+                        <span id="image-loader">
+                           <img alt="" src="images/loader.gif" />
+                        </span>
+                     </div>
+                  </fieldset>
+                  </form>
+
+            <div id="message-warning"> Error boy</div>
+                  <div id="message-success">
+                     <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+                  </div>
             </div>
 
+
+               <aside className="four columns footer-widgets">
+                  <div className="widget widget_contact">
+
+                     <h4>Feel free to send me a message.</h4>
+                     
+                  </div>
+               </aside>
          </div>
-
-         <div className="row">
-            <div className="eight columns">
-
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
-
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
-
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>Feel free to send me a message</h4>
-					   
-				   </div>
-            </aside>
-      </div>
-   </section>
+      </section>
     );
+  }
+  onNameChange(event) {
+	this.setState({name: event.target.value})
+  }
+
+  onEmailChange(event) {
+	this.setState({email: event.target.value})
+  }
+
+  onMessageChange(event) {
+	this.setState({message: event.target.value})
   }
 }
 
